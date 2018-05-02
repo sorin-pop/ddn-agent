628932a942d426fa60b657ac07a98a85f2cd3f76
Daniel Javorszky <daniel.javorszky@liferay.com>
2018-05-02 11:46:53 +0200

    Update deploy scripts

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -5,20 +5,20 @@ if [[ $# -ne 1 ]]; then
 else
     version=$1
     # rootloc is the base root of the repository
-    rootloc=`pwd`/../../
+    rootloc=`pwd`/../
 
-    echo "building binary of server.."
-    cd $rootloc/agent
+    echo "building binary of agent.."
+    cd $rootloc
     GOOS=linux go build -ldflags "-X main.version=`date -u +%Y%m%d.%H%M%S`"
 
-    cd $rootloc/dist/agent
+    cd $rootloc/dist
 
     for d in */; do
         dir=${d%/}
 
         echo "Creating $dir"
 
-        cp -r $rootloc/agent/agent $rootloc/agent/sql $dir
+        cp -r $rootloc/ddn-agent $rootloc/sql $dir
 
         docker build -t agent-$dir:$version -t agent-$dir:latest $dir
 
@@ -27,7 +27,7 @@ else
         docker tag agent-$dir:latest djavorszky/ddn-agent-$dir:latest
         docker push djavorszky/ddn-agent-$dir:latest
 
-        rm -rf $dir/agent $dir/sql
+        rm -rf $dir/ddn-agent $dir/sql
 
         echo "Created $dir"
     done
