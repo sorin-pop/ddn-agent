4aa346c66fd52521940e09a9721af4912e6719b0
Daniel Javorszky <daniel.javorszky@liferay.com>
2018-05-23 15:44:46 +0200

    Remove failing script execution

--- sql/oracle/grant_user.sql
+++ sql/oracle/grant_user.sql
@@ -1,8 +1,6 @@
 WHENEVER OSERROR EXIT FAILURE
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
-@$ORACLE_HOME/rdbms/admin/catexf.sql
-
 grant select on dba_datapump_jobs to &1;
 grant create any directory to &1;
 grant create EXTERNAL JOB to &1;
